ff7fa197d5bf33b1120ee31c48315effaac90333
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-12-18 14:10:27 -0300

    attempting to package leela-chess-zero

---
+++ packages/leela-chess-zero.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  leela-chess-zero = pkgs.fetchFromGitHub {
+    owner = "LeelaChessZero";
+    repo = "lc0";
+    rev = "afdd67c2186f1f29893d495750661a871f7aa9ac";
+    sha256 = "sha256-VUTUTYvvE2kGhL1EDmidV2J4/NcNcJb3oh81jsYJyfo=";
+    fetchSubmodules = true;
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "leela-chess-zero";
+  src = leela-chess-zero;
+  buildInputs = with pkgs; [
+    git
+    python3
+    eigen
+    openblas
+    zlib
+    gtest
+    ninja
+  ];
+  nativeBuildInputs = with pkgs; [
+    meson
+    pkg-config
+  ];
+  mesonFlags = [
+    "-Dopenblas_libdirs=${pkgs.openblas.out}/lib"
+    "-Dopenblas_include=${pkgs.openblas.dev}/include"
+    "-Dblas=true"
+    "-Dmetal=disabled"
+  ];
+}
